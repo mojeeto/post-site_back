d99a970953d703138378b006c2465ec5614bd8d7
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-18 21:39:24 +0330

    add parsermiddleware

---
+++ middleware/parserMiddleware.ts
@@ -0,0 +1,28 @@
+import bodyParser from "body-parser";
+import multer from "multer";
+import { Router } from "express";
+import path from "path";
+
+export const validTypes = ["image/jpg", "image/jpeg", "image/png"];
+
+const parserMiddleware = Router();
+
+parserMiddleware.use(bodyParser.json());
+parserMiddleware.use(
+  multer({
+    fileFilter: (req, file, cb) => {
+      if (!validTypes.includes(file.mimetype)) cb(null, false);
+      cb(null, true);
+    },
+    storage: multer.diskStorage({
+      filename(req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+      },
+      destination(req, file, cb) {
+        cb(null, path.join(require.main!.path, "images"));
+      },
+    }),
+  }).single("postImage")
+);
+
+export default parserMiddleware;
